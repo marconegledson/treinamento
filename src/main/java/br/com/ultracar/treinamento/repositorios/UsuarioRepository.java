bc65340297b928704f8bca28cb86d2f9bcaa341a
Marcone Almeida <marconegledson@gmail.com>
2019-06-07 16:57:23 -0300

    segunda aula

---
+++ src/main/java/br/com/ultracar/treinamento/repositorios/UsuarioRepository.java
@@ -0,0 +1,34 @@
+package br.com.ultracar.treinamento.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.ultracar.treinamento.entidades.Operacao;
+import br.com.ultracar.treinamento.entidades.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	@Query("Select u From Usuario u Where u.login = :login ")
+	public Usuario findUsuarioByLogin(String login);
+	
+	@Query("Select u From Usuario u Where u.senha = :senha ")
+	public Usuario findUsuarioBySenha(String senha);
+	
+	@Query(		"Select operacoes From Usuario usuario "
+			+ 	"Inner Join usuario.permissoesDeAcesso permissoesDeAcesso "
+			+	"Inner Join permissoesDeAcesso.operacoes operacoes "
+			+	"Where usuario = :usuario ")
+	public List<Operacao> findOperacaoByUsuario(Usuario usuario);
+	
+	@Query(		"Select operacoes From Usuario usuario "
+			+ 	"Inner Join usuario.permissoesDeAcesso permissoesDeAcesso "
+			+	"Inner Join permissoesDeAcesso.operacoes operacoes "
+			+	"Where usuario.id = :idUsuario ")
+	public List<Operacao> findOperacaoByUsuario(@Param("idUsuario") Long id);
+
+}
---
+++ src/main/java/br/com/ultracar/treinamento/servicos/UsuarioService.java
@@ -0,0 +1,37 @@
+package br.com.ultracar.treinamento.servicos;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ultracar.treinamento.entidades.Usuario;
+import br.com.ultracar.treinamento.repositorios.UsuarioRepository;
+
+@Service
+@Transactional(propagation = Propagation.REQUIRED)
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repositorio;
+	
+	public void salvarUsuario(Usuario usuario) {
+		this.repositorio.save(usuario);
+	}
+	
+	public void deletarUsuario(Usuario usuario) {
+		this.repositorio.deleteById(usuario.getId());
+	}
+	
+	public void deletarMuitosUsuario(List<Long> ids) {
+		ids.parallelStream().forEach(id -> {
+			if(this.repositorio.existsById(id)) {
+				this.repositorio.deleteById(id);
+			}
+		});
+	}
+
+}
